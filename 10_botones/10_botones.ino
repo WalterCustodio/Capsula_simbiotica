a95353a0fb3eb3842a0646ae04c13d512f18e717
WalterCustodio <82871842+WalterCustodio@users.noreply.github.com>
2025-10-22 19:40:22 -0300

    Actualizar 10_botones.ino

--- 10_botones/10_botones.ino
+++ 10_botones/10_botones.ino
@@ -1,29 +1,34 @@
-// --- Arduino: 10 botones (5 alfombra + 4 mesa + 1 botón "0") ---
-const int numBotones = 10;
-int pines[numBotones] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; // 10 botones
-int estadoActual[numBotones];
-int estadoPrevio[numBotones];
+// --- Arduino: 10 botones (5 alfombra + 4 mesa + 1 botón "0") --- 
+const int numBotones = 10; 
+int pines[numBotones] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; // 10 botones 
+int estadoActual[numBotones]; 
+int estadoPrevio[numBotones]; 
 
-void setup() {
-  Serial.begin(115200);
-  for (int i = 0; i < numBotones; i++) {
-    pinMode(pines[i], INPUT_PULLUP); // usa pull-up interno (botón a GND)
-    estadoPrevio[i] = digitalRead(pines[i]);
-  }
-}
+void setup() { 
+  Serial.begin(115200); 
+  for (int i = 0; i < numBotones; i++) { 
+    pinMode(pines[i], INPUT_PULLUP); // usa pull-up interno (botón a GND) 
+    estadoPrevio[i] = digitalRead(pines[i]); 
+  } 
+} 
 
-void loop() {
-  for (int i = 0; i < numBotones; i++) {
-    estadoActual[i] = digitalRead(pines[i]);
+void loop() { 
+  for (int i = 0; i < numBotones; i++) { 
+    estadoActual[i] = digitalRead(pines[i]); 
 
-    if (estadoActual[i] != estadoPrevio[i]) { // cambio detectado
-      int estado = (estadoActual[i] == LOW) ? 1 : 0; // invertido por pull-up
-      Serial.print("B");
-      Serial.print(i);
-      Serial.print(":");
-      Serial.println(estado);
-      estadoPrevio[i] = estadoActual[i];
-      delay(10);
-    }
-  }
+    if (estadoActual[i] != estadoPrevio[i]) { // cambio detectado 
+      if (estadoActual[i] == LOW) {
+        Serial.print("Botón ");
+        Serial.print(i);
+        Serial.println(" activo");
+      } else {
+        Serial.print("Botón ");
+        Serial.print(i);
+        Serial.println(" inactivo");
+      }
+
+      estadoPrevio[i] = estadoActual[i]; 
+      delay(10); // pequeño debounce
+    } 
+  } 
 }
